1e3d749182e2f97220914890f74b9e90246ac416
KhuiKhui <khoikhuat0807@gmail.com>
2024-12-07 00:17:37 +0700

    feat: add input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div className='w-full h-10 rounded-lg border-2 border-black flex items-center px-5'>
+        <input placeholder='Enter you question...' className='size-fit outline-none'></input>
+    </div>
+  )
+}
+
+export default Input
