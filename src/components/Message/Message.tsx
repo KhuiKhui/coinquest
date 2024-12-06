b56dfc55e38975f140ac4d798af1d8ea28d0fbc2
KhuiKhui <khoikhuat0807@gmail.com>
2024-12-07 00:40:07 +0700

    feat: add message and fix a few bugs

---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface MessageProps extends HTMLDivElement {
+    content: string,
+}
+
+function Message({content, className} : MessageProps) {
+  return (
+    <div className={twMerge(className, "text-wrap px-12 py-5 rounded-full bg-slate-200 border border-black text-black size-fit")}>{content}</div>
+  )
+}
+
+export default Message
