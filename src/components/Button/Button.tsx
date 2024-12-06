72ee46c7ef1ef6b6f80d11810b08b861331bd66a
KhuiKhui <khoikhuat0807@gmail.com>
2024-12-07 00:00:35 +0700

    feat: add button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 interface ButtonProps extends HTMLDivElement {
   content: string;
 }
 
 function Button({ content }: ButtonProps) {
   return (
-    <div className="p-2 bg-white border border-beige w-fit">{content}</div>
+    <div className="p-2 bg-white border border-beige w-fit h-fit rounded-md hover:bg-dust-white transition-all cursor-pointer">{content}</div>
   );
 }
 
